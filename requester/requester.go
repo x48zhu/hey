0c473be1f263df4adc022ff25287700f26886399
Xi Zhu <xzhu@squareup.com>
2023-06-09 09:59:31 -0700

    Enforce QPS

--- requester/requester.go
+++ requester/requester.go
@@ -217,18 +217,26 @@ func (b *Work) runWorker(client *http.Client, n int) {
 			return http.ErrUseLastResponse
 		}
 	}
+
+	var wg sync.WaitGroup
 	for i := 0; i < n; i++ {
 		// Check if application is stopped. Do not send into a closed channel.
 		select {
 		case <-b.stopCh:
+			wg.Wait()
 			return
 		default:
 			if b.QPS > 0 {
 				<-throttle
 			}
-			b.makeRequest(client)
+			wg.Add(1)
+			go func() {
+				b.makeRequest(client)
+				wg.Done()
+			}()
 		}
 	}
+	wg.Wait()
 }
 
 func (b *Work) runWorkers() {
@@ -252,8 +260,10 @@ func (b *Work) runWorkers() {
 	}
 	client := &http.Client{Transport: tr, Timeout: time.Duration(b.Timeout) * time.Second}
 
+	wait := time.Duration(1000/b.C) * time.Millisecond
 	// Ignore the case where b.N % b.C != 0.
 	for i := 0; i < b.C; i++ {
+		time.Sleep(wait)
 		go func() {
 			b.runWorker(client, b.N/b.C)
 			wg.Done()
